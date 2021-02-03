9ff4c7b1967e54e40613aecbc1e24d5612fd2661
Parth <pcs003@ucsd.edu>
2021-02-03 16:17:13 -0500

    continue making controller

--- first_routes/app/controllers/users_controller.rb
+++ first_routes/app/controllers/users_controller.rb
@@ -5,10 +5,26 @@ class UsersController < ApplicationController
     end
 
     def create
-        render json: params
+        user = User.new(user_params)
+        # replace the `user_attributes_here` with the actual attribute keys
+        if user.save
+            render json: user
+        else
+            render json: user.errors.full_messages, status: :unprocessable_entity
+        end
     end
 
     def show
-        render json: params
+        user = User.find_by(id: params[:id])
+        if user
+            render json: user
+        else
+            render plain: "Could not find user"
+    end
+
+    def
+
+    def user_params
+        params.require(:user).permit(:name, :email)
     end
 end
